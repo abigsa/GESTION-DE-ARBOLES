97128f50c7882ca9db5421fb11b595a976516e03
Kevin <kev.aar_on@hotmail.es>
2026-03-18 23:41:14 -0600

    creacion backend

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const oracledb = require("oracledb");
+require("dotenv").config({ path: "../.env" });
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectString: process.env.DB_CONNECT_STRING
+};
+
+async function getConnection() {
+  return await oracledb.getConnection(dbConfig);
+}
+
+module.exports = {
+  getConnection,
+  oracledb
+};
